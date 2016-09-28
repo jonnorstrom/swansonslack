4008bfd40c657b0f6493d8585b0cc4b0aaae0c0c
Jonathan Norstrom <jon.norstrom@gmail.com>
2016-09-27 22:19:00 -0500

    string

--- app.rb
+++ app.rb
@@ -5,15 +5,14 @@ require 'uri'
 require_relative 'quotes'
 
 post '/swanson' do
-  get_quote
-  # send_quote_to_slack(params[:response_url], get_quote)
+  send_quote_to_slack(params[:response_url], get_quote)
 end
 
 def send_quote_to_slack(response_url, quote)
   uri = URI(response_url)
-  req = Net::HTTP::Post.new(uri, {'Content-Type' =>'application/json'})
+  req = Net::HTTP::Post.new(uri)
 
-  req.body = format_body(quote)
+  req.body = quote
 
   Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
     http.request(req)
